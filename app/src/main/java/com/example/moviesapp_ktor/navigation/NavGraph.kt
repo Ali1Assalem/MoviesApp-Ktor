e840cb1ae3cfd9e80d5c462f2fcf7bda084ff0f5
ALI <ali11salem00@gmail.com>
2022-08-24 15:21:30 +0300

    Setup Navigation

---
+++ app/src/main/java/com/example/moviesapp_ktor/navigation/NavGraph.kt
@@ -0,0 +1,38 @@
+package com.example.moviesapp_ktor.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.moviesapp_ktor.util.Constants
+
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Splash.route
+    ) {
+        composable(route = Screen.Splash.route) {
+
+        }
+        composable(route = Screen.Welcome.route) {
+
+        }
+        composable(route = Screen.Home.route) {
+
+        }
+        composable(
+            route = Screen.Details.route,
+            arguments = listOf(navArgument(Constants.DETAILS_ARGUMENT_KEY) {
+                type = NavType.IntType
+            })
+        ) {
+
+        }
+        composable(route = Screen.Search.route) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/moviesapp_ktor/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.moviesapp_ktor.util
+
+object Constants {
+
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+
+    const val HERO_DATABASE_TABLE = "hero_table"
+    const val HERO_REMOTE_KEY_DATABASE_TABLE = "hero_remote_key_table"
+
+}
