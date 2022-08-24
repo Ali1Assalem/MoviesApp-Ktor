0d24336a0a37a21727713c97cc65724ee5fa46e7
ALI <ali11salem00@gmail.com>
2022-08-24 15:25:26 +0300

    Setup Navigation

--- app/src/main/java/com/example/moviesapp_ktor/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp_ktor/MainActivity.kt
@@ -3,38 +3,22 @@ package com.example.moviesapp_ktor
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.moviesapp_ktor.navigation.SetupNavGraph
 import com.example.moviesapp_ktor.ui.theme.MoviesAppKtorTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MoviesAppKtorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MoviesAppKtorTheme() {
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MoviesAppKtorTheme {
-        Greeting("Android")
     }
 }
